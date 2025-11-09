1a16725d1cb857c77da1a16bcc44a8b20442528b
Abdurashid Abdumajidov <abdumajidovabdurashid572@gmail.com>
2025-11-09 11:58:50 +0500

    6-commit

--- src/Roots/RootLayOut/RootLayOut.jsx
+++ src/Roots/RootLayOut/RootLayOut.jsx
@@ -2,16 +2,33 @@ import { NavLink, Outlet } from "react-router-dom"
 import "./RootLayOut.scss"
 
 
-import React from 'react'
+import { useState, useEffect } from "react"
 
 const RootLayOut = () => {
+
+     const [isfixed, setIsfixed] = useState(false)
+
+  useEffect(() => {
+    const handelScrol = () => {
+      if(window.scrollY > 50){
+        setIsfixed(true)
+      }else{
+        setIsfixed(false)
+      }
+    }
+    window.addEventListener("scroll", handelScrol)
+    return () => window.removeEventListener("scroll", handelScrol)
+  }, [])
+
+
+
   return (
     <div className="rootLayOut">
         <h1 className="rootLayOut-title">Aka market 5</h1>
         <div className="rootLayOut-sides">
 
 
-            <div className="rootLayOut-sides-up container">
+            <div className= {`rootLayOut-sides-up ${ isfixed ? "fixed" : ""} `}>
                 <nav>
                     <NavLink className="btn" to="/TotalBase" >baza</NavLink>
                     
